b57a14ba16f1ab198fc6e61597d8365edff94a11
HUAWEI-Ubuntu <ruluy0205@163.com>
2024-06-05 10:52:49 +0800

    finish exercise 5-6

--- code/exercise-answer/5-6/test.s
+++ code/exercise-answer/5-6/test.s
@@ -3,11 +3,39 @@
     .text
     .global _start
 
-_start:
+# #define set_struct(s) \
+#  s.a = a; \
+#  s.b = b;
+# #define get_struct(s) \
+#  a = s.a; \
+#  b = s.b;
 
+.macro get_struct S_s
+    lw x6, 0(\S_s)
+    lw x7, 4(\S_s)
+.endm
+
+.macro set_struct S_s
+    sw x6, 0(\S_s)
+    sw x7, 4(\S_s)
+.endm
+
+
+_start:
+    li x6, 0x12345678
+    li x7, 0x87654321
+    la x5, struct_S_s
+
+    set_struct x5
+    li x6, 0
+    li x7, 0
+    get_struct x5
 
 stop:
     j stop
 
+struct_S_s:
+    .word 0
+    .word 0
 
     .end
